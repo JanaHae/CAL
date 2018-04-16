f6255c03fc6d41f2a665612e847c1b235fa6d090
Jana Ha <jana.haenssler@gmx.de>
2018-04-17 00:15:06 +0200

    commenting out the python scripts

--- NER Tagger/Split_Tags.py
+++ NER Tagger/Split_Tags.py
@@ -5,7 +5,6 @@
 filename='train.txt'
 with open(filename) as f:
     lines = f.readlines()
-#print("Done reading")
 print('STATUS: Read file {}'.format(filename))
 
 i = 0 #counter
@@ -13,31 +12,22 @@ docPERS = '' #output 1, only PERS
 docTIT = '' #output 1, only TIT
 docLOC = '' #output 1, only LOC
 
-#print(type(lines[i])) #string
-#print(lines[i]) #Kg.<TAB>B-PERS<TAB>B-TIT
-
 for i in range(0, len(lines)-1): #zero-based index
-    #print(len(lines[i]))
     if '\t' in lines[i]:
         # Find first Tab
         firstTab = lines[i].find('\t')
-        #print('FirstTab at index: {}'.format(firstTab))
 
         # Save the key / word / token
         key = lines[i][:firstTab]
-        #print('Key: {}, Laenge: {}'.format(key, len(key)))
 
         # Find last Tab
         lastTab = lines[i].rfind('\t')
-        #print('LastTab at index: {}'.format(lastTab))
 
         # Save Value1 / v1 / first Label
         value1 = lines[i][firstTab+1:lastTab]
-        #print('value 1: {}, Laenge: {}'.format(value1, len(value1)))
 
         # Save Value2 / v2 / second Label
-        value2 = lines[i][lastTab+1:len(lines[i])-1]  #-1 am Ende wegen des Umbruchchars \n
-        #print('value 2: {}, Laenge: {}'.format(value2, len(value2)))
+        value2 = lines[i][lastTab+1:len(lines[i])-1]  #-1 at the end because of char \n at the end
         secondLabel = ''
 
         if 'PERS' in value1:
@@ -72,9 +62,5 @@ writeFile(docPERS, 'TrainDocPERS.txt')
 writeFile(docLOC, 'TrainDocLOC.txt')
 writeFile(docTIT, 'TrainDocTIT.txt')
 
-#print('---------------\n')
-#print(docPERS)
-#print('---------------')
-#print(len(docPERS))
 
 
